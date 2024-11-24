7931fea1a222c7c83ba3e58407ad5434b7ac3b42
SaedEmad <alrababah.saedemad@gmail.com>
2024-11-24 08:07:47 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+// App.tsx
+import { View } from "react-native";
+import "./global.css";
+import Texting from "./app/index";
+
+export default function App() {
+  return (
+    <View>
+      <Texting />
+    </View>
+  );
+}
