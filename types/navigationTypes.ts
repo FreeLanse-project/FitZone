ed40c11fd97a6a9966d17e3b2191d42b2c04a818
SaedEmad <alrababah.saedemad@gmail.com>
2024-11-25 09:19:31 +0300

    add types

--- types/navigationTypes.ts
+++ types/navigationTypes.ts
@@ -1,6 +1,8 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
+  navigate(arg0: string): void;
   WelcomePage: undefined;
+  SignIn: undefined;
   SignUp: undefined;
 };
